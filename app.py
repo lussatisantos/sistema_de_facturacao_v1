692c2a52a0f57c5166036e0e12202aa72e5d7ad8
Lussati Santos <lussatisantos10@gmail.com>
2023-07-22 17:28:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from time import sleep
 
+
+s = fecho = 0
+
 print ('-=' * 20)
 print('SISTEMA DE FACTURACAO VERSAO 1.0')
 print ('-=' * 20)
@@ -40,24 +43,35 @@ while opcao != 5:
             print('Nome: {} '.format(nome))
             print('Produto: {}' .format(produto))
             print('Desconto: {}' .format(desconto))
-            print('Total a pagar: {} Kz' .format((preco - desconto) + (preco * 0.14)))
+            n = (preco - desconto) + (preco * 0.14)
+            fecho += n
+            print('Total a pagar: {} Kz' .format(n))
             print('Acrescentando IVA de 14%')
             print('-=' * 20)
             print('OBRIGADO PELA PREFERENCIA')
             print('-=' * 20)
-
             novoregistro = str(input('Desejas registrar novamente uma compra? [S/N]: ')).upper().strip()
 
     if opcao == 2:
         nome = str(input('Nome do Cliente: ')).strip()
-        produto = str(input('Digite o produto: ')).strip()
-        preco = float(input('Digite o preco: '))
-        item = str(input('Adicionar mais um produto [S/N]: ')).upper().strip()
-        if item == 'N':
-            print('Emtindo a factura...')
-            sleep(3)
-            print ('-=' * 20)
-            print('SISTEMA DE FACTURACAO VERSAO 1.0')
-            print ('-=' * 20)
-            print('Nome: {}' .format(nome))
-            
+        while True:
+            produto = str(input('Digite o produto: ')).strip()
+            preco = float(input('Digite o preco: '))
+            s += preco
+            item = str(input('Adicionar mais um produto [S/N]: ')).upper().strip()
+            if item == 'N':
+                break
+
+        print('Emtindo a factura...')
+        sleep(2)
+        print()
+        print ('-=' * 20)
+        print('SISTEMA DE FACTURACAO VERSAO 1.0')
+        print ('-=' * 20)
+        print('Nome: {}' .format(nome))
+        print('Produtos: Variados')
+        print('Total a pagar: {}' .format(s))
+        print('Sem aplicacao do IVA')
+        print('-'*20)
+        print('OBRIGADO PELA PREFERENCIA')
+        print('-'*20)
